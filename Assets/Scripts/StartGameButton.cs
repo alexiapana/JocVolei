28e90850a46c2e3a182dd95022f723b3b82c5d3c
alexiapana <alexia.pana@student.unitbv.ro>
2024-04-13 16:34:45 +0300

    am adaugat o pagina de start si una pentru controls

---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameButton : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("FirstScene");
+    }
+
+    public void ShowControls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
